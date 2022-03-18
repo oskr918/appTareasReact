dbcc1021581d11b1c96e8da1cb2cbd45656c065f
oscar <camamoblamientos@gmail.com>
2022-03-18 18:06:02 -0300

    first commit

---
+++ src/componentes/ListaDeTareas.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import TareaFormulario from './TareaFormulario'
+import '../hojas-de-estilo/ListaDeTareas.css'
+import Tarea from './Tarea'
+
+const ListaDeTareas = () => {
+
+  const [tareas, setTareas] = useState([]);
+
+  const agregarTarea = tarea =>{
+    if(tarea.texto.trim()){
+      console.log()
+      tarea.texto = tarea.texto.trim();
+       const tareasActualizadas = [tarea, ...tareas]
+       setTareas(tareasActualizadas);
+    }
+  }
+
+  const eliminarTarea = id =>{
+    const tareasActualizadas =  tareas.filter(tarea => tarea.id !==id);
+    setTareas(tareasActualizadas);
+  }
+  return (
+    <>
+        <TareaFormulario onSubmit={agregarTarea}></TareaFormulario>
+        <div className='tareas-lista-contenedor'>
+           {
+             tareas.map((tarea)=>
+               <Tarea
+                  key={tarea.id}
+                  id={tarea.id}
+                  texto={tarea.texto}
+                  completada={tarea.completada}
+                  eliminarTarea={eliminarTarea}
+               />
+                  
+             )
+           }
+        </div>
+    </>
+  )
+}
+
+export default ListaDeTareas
